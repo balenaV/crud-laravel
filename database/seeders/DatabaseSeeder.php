62859365c395fb7214746e192cfc6193211c8ac0
Victor Balena <victorbalenaes@gmail.com>
2025-08-05 17:35:55 -0300

    feat: adiciona factory de produtos para banco de dados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(ProductSeeder::class);
+        \App\Models\Product::factory(10)->create();
+        // $this->call(ProductSeeder::class);
     }
 }
